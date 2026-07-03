0c1c14ed91960a3c9f100315173b5ebe0118acf6
seham <daborseham31@gmail.com>
2026-07-03 15:35:32 +0300

    resolved layout conflicts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,10 +6,14 @@ export default function RootLayout() {
     <>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
+<<<<<<< Updated upstream
         <Stack.Screen name="signup" />
         <Stack.Screen name="home" />
         <Stack.Screen name="forgot-password" />
         <Stack.Screen name="admin" />
+=======
+        <Stack.Screen name="home" />
+>>>>>>> Stashed changes
       </Stack>
       <StatusBar style="auto" />
     </>
